6c8f324f3d6f7dd74e51671b6b398ac6cb570456
BrinLewis <brinsley22@hotmail.co.uk>
2023-02-16 17:26:36 +0000

    Add event listener for sidebar toggle button

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ import "./assets/home.svg";
 import "./assets/plus.svg";
 import "./assets/close-blue.svg";
 import "./assets/close-red.svg";
+
+const sidebarToggle = document.getElementById("sidebar-toggle");
+const sidebar = document.querySelector(".sidebar");
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("closed");
+});
