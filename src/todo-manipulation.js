7c4156ba37998541e517551874ee1bf0d1377b39
BrinLewis <brinsley22@hotmail.co.uk>
2023-02-20 18:28:13 +0000

    Add function to retrieve element's related object and Add changePriority function which changes color and object property

--- src/todo-manipulation.js
+++ src/todo-manipulation.js
@@ -1,4 +1,6 @@
-export { expandTodo, toggleTodoChecked, changePriorityColor };
+import { allTodos } from "./todo-creator";
+
+export { expandTodo, toggleTodoChecked, changePriority, };
 
 function expandTodo(todo) {
   todo.classList.toggle("expanded");
@@ -15,16 +17,14 @@ function toggleTodoChecked(box) {
       "style",
       "text-decoration: line-through; color: rgb(90, 90, 90);"
     );
-    date.readOnly = true;
-    date.setAttribute("style", "background-color: rgba(209, 209, 209, 0.651)");
+    date.disabled = true;
   } else {
     todo.setAttribute("style", "background-color: white");
     infoContainer.removeAttribute(
       "style",
       "text-decoration: line-through; color: black;"
     );
-    date.readOnly = false;
-    date.setAttribute("style", "background-color: none");
+    date.disabled = false;
   }
 }
 
@@ -42,4 +42,28 @@ function changePriorityColor(priorityBtn) {
     default:
       priorityBtn.style.backgroundColor = "red";
   }
+}
+
+function getRelatedObject(element) {
+  let todoContainer;
+  if (element.classList.contains("todo")) {
+    todoContainer = element;
+  } else if (element.classList.contains("info-item")) {
+    todoContainer = (element.parentNode).parentNode;
+  } else {
+    todoContainer = element.parentNode;
+  }
+
+  const todoId = todoContainer.id;
+  const index = todoId.slice(-1) - 1;
+  let object = allTodos[index];
+
+  return object;
+}
+
+function changePriority(priorityBtn) {
+  changePriorityColor(priorityBtn);
+
+  let object = getRelatedObject(priorityBtn);
+  object.priority = priorityBtn.style.backgroundColor;
 }
