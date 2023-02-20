937965e40e710375281893f796502c0403943b2d
BrinLewis <brinsley22@hotmail.co.uk>
2023-02-20 16:47:32 +0000

    Move sidebar logic into separate module

---
+++ src/sidebar-logic.js
@@ -0,0 +1,9 @@
+export { sidebarToggle }
+
+function sidebarToggle() {
+  const toggleBtn = document.getElementById("sidebar-toggle");
+  const sidebar = document.querySelector(".sidebar");
+  toggleBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("closed");
+  });
+}
