0db92839c69730e0be9088816b15e603a6336550
BrinLewis <brinsley22@hotmail.co.uk>
2023-02-23 15:38:27 +0000

    Module now handles todo event listeners

--- src/todo-creator.js
+++ src/todo-creator.js
@@ -1,4 +1,11 @@
-export { allTodos, createTodo, renderTodos}
+import {
+  expandTodo,
+  toggleTodoChecked,
+  changePriority,
+  updateText,
+} from "./todo-manipulation";
+
+export { allTodos, createTodo, renderTodos };
 
 const allTodos = [];
 
@@ -8,19 +15,20 @@ function createTodo(title, description, dueDate, priority) {
     description,
     dueDate,
     priority,
-  }
+  };
 
   allTodos.push(todo);
-
+  renderTodos();
   return todo;
 }
 
 function renderTodos() {
+  clearTodos();
   allTodos.forEach((item, index) => {
     // Create elements
     const todoContainer = document.createElement("div");
     todoContainer.classList.add("todo");
-    todoContainer.id = `todo-item-${index + 1}`
+    todoContainer.id = `todo-item-${index + 1}`;
 
     const priorityIndicator = document.createElement("div");
     priorityIndicator.classList.add("priority");
@@ -49,7 +57,7 @@ function renderTodos() {
     description.textContent = item.description;
 
     const date = document.createElement("input");
-    date.setAttribute("type", "date")
+    date.setAttribute("type", "date");
     date.setAttribute("name", `duedate-${index + 1}`);
     date.setAttribute("id", `duedate-${index + 1}`);
     date.setAttribute("value", `${item.dueDate}`);
@@ -60,8 +68,8 @@ function renderTodos() {
     deleteBtn.classList.add("delete-todo");
 
     // Structure elements
-    const content = document.querySelector(".content");
-    content.appendChild(todoContainer);
+    const todoList = document.querySelector(".todo-list");
+    todoList.appendChild(todoContainer);
 
     const todoContainerChildren = [
       priorityIndicator,
@@ -72,11 +80,72 @@ function renderTodos() {
       deleteBtn,
     ];
 
-    todoContainerChildren.forEach(element => {
+    todoContainerChildren.forEach((element) => {
       todoContainer.appendChild(element);
-    })
+    });
 
     infoContainer.appendChild(title);
     infoContainer.appendChild(description);
+  });
+
+  todoEventListeners();
+}
+
+function clearTodos() {
+  const todoList = document.querySelector(".todo-list");
+
+  while (todoList.firstChild) {
+    todoList.removeChild(todoList.lastChild);
+  }
+}
+
+function todoEventListeners() {
+  // Expand - Event Listener
+  const allTodoElements = document.querySelectorAll(".todo");
+  allTodoElements.forEach((todo) => {
+    const infoContainer = todo.querySelector(".todo-info");
+
+    todo.addEventListener("click", (event) => {
+      if (todo !== event.target && infoContainer !== event.target) {
+        return;
+      }
+      expandTodo(todo);
+    });
+  });
+
+  // Checkbox - Event Listener
+  const allCheckboxes = document.querySelectorAll("input[type='checkbox']");
+  allCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener("click", () => {
+      toggleTodoChecked(checkbox);
+    });
+  });
+
+  // Delete btn color change - Event Listener
+  const allDeleteTodoBtns = document.querySelectorAll(".delete-todo");
+  allDeleteTodoBtns.forEach((btn) => {
+    btn.addEventListener("mouseover", () => {
+      btn.src = "./assets/close-red.svg";
+    });
+
+    btn.addEventListener("mouseout", () => {
+      btn.src = "./assets/close-blue.svg";
+    });
+  });
+
+  // Priority color - Event Listener
+  const allPriorityBtns = document.querySelectorAll(".priority");
+  allPriorityBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      changePriority(btn);
+    });
+  });
+
+  // Update object info with inputted DOM info - Event Listener
+  const allInfoItems = document.querySelectorAll(".info-item");
+  allInfoItems.forEach((item) => {
+    item.addEventListener("blur", () => {
+      updateText(item);
+    });
   });
 }
