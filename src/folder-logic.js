7d4bcca2883c93334c9e8d7efe48eb798d0bf49c
BrinLewis <brinsley22@hotmail.co.uk>
2023-02-23 17:06:14 +0000

    Refine plan for implementing folders

--- src/folder-logic.js
+++ src/folder-logic.js
@@ -1,9 +1,9 @@
-const folderNames = [];
+const folderNames = ["Home", "Daily", "Weekly", "Homework"];
 
 // The folder names list will just be an array containing strings of each folder
 // name. 
 //
-//The folder names will be rendered as li's in an unordered list, each of
+// The folder names will be rendered as li's in an unordered list, each of
 // which will have a click listener that will take the textContent of the li
 // that was clicked and call renderTodos, which will take the textContent, and 
 // render only the todo objects which have the key:value pair of
@@ -17,11 +17,7 @@ const folderNames = [];
 // which will take the value of the text input and add an li with that value as
 // its textContent. addNewFolder will also use folderNames.push(newFolderName)
 // to add it to the array.
-//
-// When you click a folder name, it will call renderTodos, which will now take
-// what folder you want, then render only the todo objects which have the 
-// key:value pair of folder:*foldername*.
 // 
-// This means that the form will need to ask what folder you'd like to put the 
-// todo you're creating in and then createTodo will take in that answer and put
+// This means that the todo creation form will need to ask what folder you'd 
+// like to put the todo in and then createTodo will take in that answer and put
 // it as a property of the object so that it can be read by renderTodos.
