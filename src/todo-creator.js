71fd343a77f6256e7bef28fae732dbd11c747851
BrinLewis <brinsley22@hotmail.co.uk>
2023-02-24 17:26:41 +0000

    renderTodos() now checks what folder to render

--- src/todo-creator.js
+++ src/todo-creator.js
@@ -25,70 +25,72 @@ function createTodo(title, description, dueDate, priority, folder) {
   return todo;
 }
 
-function renderTodos() {
+function renderTodos(folderToRender = "Home") {
   clearTodos();
   allTodos.forEach((item, index) => {
-    // Create elements
-    const todoContainer = document.createElement("div");
-    todoContainer.classList.add("todo");
-    todoContainer.id = `todo-item-${index + 1}`;
+    if (folderToRender === "Home" || folderToRender === item.folder) {
+      // Create elements
+      const todoContainer = document.createElement("div");
+      todoContainer.classList.add("todo");
+      todoContainer.id = `todo-item-${index + 1}`;
 
-    const priorityIndicator = document.createElement("div");
-    priorityIndicator.classList.add("priority");
-    priorityIndicator.style.backgroundColor = item.priority;
+      const priorityIndicator = document.createElement("div");
+      priorityIndicator.classList.add("priority");
+      priorityIndicator.style.backgroundColor = item.priority;
 
-    const checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.id = `checkbox-${index + 1}`;
+      const checkbox = document.createElement("input");
+      checkbox.setAttribute("type", "checkbox");
+      checkbox.id = `checkbox-${index + 1}`;
 
-    const checkboxLabel = document.createElement(`label`);
-    checkboxLabel.setAttribute("for", `${checkbox.id}`);
+      const checkboxLabel = document.createElement(`label`);
+      checkboxLabel.setAttribute("for", `${checkbox.id}`);
 
-    const infoContainer = document.createElement("div");
-    infoContainer.classList.add("todo-info");
+      const infoContainer = document.createElement("div");
+      infoContainer.classList.add("todo-info");
 
-    const title = document.createElement("p");
-    title.classList.add("todo-title");
-    title.classList.add("info-item");
-    title.setAttribute("contenteditable", true);
-    title.textContent = item.title;
+      const title = document.createElement("p");
+      title.classList.add("todo-title");
+      title.classList.add("info-item");
+      title.setAttribute("contenteditable", true);
+      title.textContent = item.title;
 
-    const description = document.createElement("p");
-    description.classList.add("todo-desc");
-    description.classList.add("info-item");
-    description.setAttribute("contenteditable", true);
-    description.textContent = item.description;
+      const description = document.createElement("p");
+      description.classList.add("todo-desc");
+      description.classList.add("info-item");
+      description.setAttribute("contenteditable", true);
+      description.textContent = item.description;
 
-    const date = document.createElement("input");
-    date.setAttribute("type", "date");
-    date.setAttribute("name", `duedate-${index + 1}`);
-    date.setAttribute("id", `duedate-${index + 1}`);
-    date.setAttribute("value", `${item.dueDate}`);
+      const date = document.createElement("input");
+      date.setAttribute("type", "date");
+      date.setAttribute("name", `duedate-${index + 1}`);
+      date.setAttribute("id", `duedate-${index + 1}`);
+      date.setAttribute("value", `${item.dueDate}`);
 
-    const deleteBtn = document.createElement("img");
-    deleteBtn.src = "./assets/close-blue.svg";
-    deleteBtn.setAttribute("alt", "delete todo button");
-    deleteBtn.classList.add("delete-todo");
+      const deleteBtn = document.createElement("img");
+      deleteBtn.src = "./assets/close-blue.svg";
+      deleteBtn.setAttribute("alt", "delete todo button");
+      deleteBtn.classList.add("delete-todo");
 
-    // Structure elements
-    const todoList = document.querySelector(".todo-list");
-    todoList.appendChild(todoContainer);
+      // Structure elements
+      const todoList = document.querySelector(".todo-list");
+      todoList.appendChild(todoContainer);
 
-    const todoContainerChildren = [
-      priorityIndicator,
-      checkbox,
-      checkboxLabel,
-      infoContainer,
-      date,
-      deleteBtn,
-    ];
+      const todoContainerChildren = [
+        priorityIndicator,
+        checkbox,
+        checkboxLabel,
+        infoContainer,
+        date,
+        deleteBtn,
+      ];
 
-    todoContainerChildren.forEach((element) => {
-      todoContainer.appendChild(element);
-    });
+      todoContainerChildren.forEach((element) => {
+        todoContainer.appendChild(element);
+      });
 
-    infoContainer.appendChild(title);
-    infoContainer.appendChild(description);
+      infoContainer.appendChild(title);
+      infoContainer.appendChild(description);
+    }
   });
 
   todoEventListeners();
@@ -131,7 +133,7 @@ function todoEventListeners() {
 
     btn.addEventListener("click", () => {
       deleteTodo(btn);
-    })
+    });
   });
 
   // Priority color - Event Listener
